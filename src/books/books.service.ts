7f2c399bba6dd1f427ca49880f3dad2cc5d7489f
ua-Slash-ua <so.slashua@gmail.com>
2025-06-22 23:29:15 +0300

    Реалізував book.service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,5 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException  } from '@nestjs/common';
 
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+}
+
+export type TypeBook = {
+    title: string;
+    author: string;
+    available: boolean;
+}
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+    private books:Book[] = []; // Тут зберігатимуться книги
+    private idCounter = 0;
+
+    create(bookData:TypeBook):Book {
+            let newBook:Book = {id: this.idCounter,...bookData}
+            this.idCounter++
+            this.books.push(newBook)
+            return newBook
+    }
+
+    findAll(): Book[] {
+        return this.books;
+    }
+
+
+    findOne(id:number):Book | null {
+        const book:Book| undefined = this.books.find(book=>book.id===id)
+        return book ?? null;
+    }
+
+    remove(id:number) {
+        const index = this.books.findIndex(book => book.id === id);
+        if (index === -1) {
+            return null;
+        }
+        const book = this.books[index];
+        this.books.splice(index, 1);
+        return book;
+    }
+}
+
