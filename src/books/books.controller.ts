a14b49f91b5b4da2f29457ce8dc2cb3adb78b3ad
ua-Slash-ua <so.slashua@gmail.com>
2025-06-22 23:37:12 +0300

    Реалізував books.controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
-import { BooksService } from './books.service';
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Post} from '@nestjs/common';
+import {Book, BooksService, TypeBook} from './books.service';
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly booksService: BooksService) {}
+    constructor(private readonly booksService: BooksService) {
+
+    }
+
+    @Get()
+    findAll(): Book[] {
+        return this.booksService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param('id',ParseIntPipe) id: number):Book|null{
+        return this.booksService.findOne(id)
+    }
+
+    @Post()
+    create(@Body() bookData:TypeBook):Book|null {
+        return this.booksService.create(bookData)
+    }
+
+    @Delete(':id')
+    remove(@Param('id',ParseIntPipe) id: number):Book|null {
+        return this.booksService.remove(id)
+    }
+
 }
